09c33515e353294b6d53087875adf21248828606
Miles544 <162893957+ngk0@users.noreply.github.com>
2024-04-28 12:11:22 -0400

    Trying to remove the need for the LKAS_HEARBIT message.

--- selfdrive/car/chrysler/carstate.py
+++ selfdrive/car/chrysler/carstate.py
@@ -290,7 +290,7 @@ class CarState(CarStateBase):
 
     if CP.carFingerprint in RAM_CARS:
       messages += CarState.get_cruise_messages()
-    else:
+    #else:
       # LKAS_HEARTBIT data needs to be forwarded!
       #forward_lkas_heartbit_messages = [
       #  ("LKAS_HEARTBIT", 10),
