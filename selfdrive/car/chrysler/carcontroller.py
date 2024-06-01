a836b0020cbbc78bd4420e511cbf033cc4416792
Miles544 <162893957+ngk0@users.noreply.github.com>
2024-06-01 13:47:03 -0400

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -47,12 +47,15 @@ class CarController(CarControllerBase):
         can_sends.append(chryslercan.create_lkas_hud(self.packer, self.CP, lkas_active, CC.hudControl.visualAlert,
                                                      self.hud_count, CS.lkas_car_model, CS.auto_high_beam))
         self.hud_count += 1
-
+    
     if ret.leftBlinker:
-      if (self.engine_startstop<100):
+        self.enginer_startstop = 0
+    
+    if self.engine_startstop < 2:
         self.last_button_frame = self.frame
-        self.engine_startstop += 1
-        can_sends.append(chryslercan.create_engine_startstop_button(self.packer, CS.button_counter + 1, 0, start_stop=True))
+        can_sends.append(chryslercan.create_engine_startstop_button(self.packer, CS.CP, 0, start_stop=True))
+    else:
+        can_sends.append(chryslercan.create_engine_startstop_button(self.packer, CS.CP, 0, start_stop=False))
 
     # steering
     if self.frame % self.params.STEER_STEP == 0:
