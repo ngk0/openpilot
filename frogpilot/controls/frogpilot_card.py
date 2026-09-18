00284b13ceb5b547357afeb6dfbfe1f638bf1b16
Nate <162893957+ngk0@users.noreply.github.com>
2026-09-17 17:37:40 -0700

    Auto-sync FrogPilot @ 1e23dec6: always-on-lateral
    Synced to upstream 'Compile FrogPilot' and re-applied the personal always-on-lateral patch at frogpilot/controls/frogpilot_card.py.

--- frogpilot/controls/frogpilot_card.py
+++ frogpilot/controls/frogpilot_card.py
@@ -95,7 +95,7 @@ class FrogPilotCard:
       self.always_on_lateral_enabled &= frogpilotCarState.alwaysOnLateralAllowed
     else:
       self.always_on_lateral_enabled |= self.car.frogpilot_toggles.always_on_lateral_main or carState.cruiseState.enabled
-      self.always_on_lateral_enabled &= carState.cruiseState.available
+      self.always_on_lateral_enabled &= True  # patched: lateral must not require the car's cruise to be on
 
     self.always_on_lateral_enabled &= carState.gearShifter not in NON_DRIVING_GEARS
     self.always_on_lateral_enabled &= sm["frogpilotPlan"].lateralCheck
